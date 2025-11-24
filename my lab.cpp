e3abddb2faf063b2304f7fd6fe6b817971ad4d44
Glaizzz <79782453+Glaizzz@users.noreply.github.com>
2025-11-25 00:25:32 +0300

    add 2d lab

---
+++ my lab.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+class Car {
+private:
+	string mark;
+	string model;
+public:
+	Car(string markc, string modelc) : mark(markc), model(modelc) {
+	}
+	~Car() {
+		cout << "| MARK: " << mark << " | MODEL: " << model << endl;
+	}
+};
+
+int main() {
+	vector<Car> cars;
+	cars.reserve(20); 
+	ifstream file("aaa.txt");
+	string mark, model;
+
+	if (!file.is_open()) {
+		cout << "err" << endl;
+		return 1;
+	}
+
+	while (file >> mark >> model) {
+		cars.emplace_back(mark, model);
+	}
+	file.close();
+	
+
+	return 0;
+}
